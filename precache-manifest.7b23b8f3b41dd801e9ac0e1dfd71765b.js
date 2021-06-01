33af7e848fb36792731ec8734c4c3df1a6beb1b6
Alkisti Lemonaki <alkisti.lemonaki.99@gmail.com>
2021-06-01 11:32:50 +0300

    Updates

---
+++ precache-manifest.7b23b8f3b41dd801e9ac0e1dfd71765b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2490d20c64ae4a696c5dd2426ba40d83",
+    "url": "/auction-app/index.html"
+  },
+  {
+    "revision": "5f4e5d0e6e29861cc07b",
+    "url": "/auction-app/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "c703c0c60df250ef71d4",
+    "url": "/auction-app/static/js/2.6b2c1e0d.chunk.js"
+  },
+  {
+    "revision": "5f4e5d0e6e29861cc07b",
+    "url": "/auction-app/static/js/main.55f2a1f9.chunk.js"
+  },
+  {
+    "revision": "f3bf5b93f7e7136135cd",
+    "url": "/auction-app/static/js/runtime-main.ad8c3a94.js"
+  }
+]);
